d9b9803e216f7486f0deec55cf93e265d999781f
Aristotelis.Liakatas <Aristotelis.Liakatas@jacobs.com>
2021-02-02 21:22:42 +0000

    Adds module to read and create polygons from text files.

--- src/main.f90
+++ src/main.f90
@@ -1,11 +1,16 @@
 
-program clip_polygon_driver
+
+    program clip_polygon_driver
+    use polygon_reader
 
     implicit none
 
     integer         :: nargs
     integer         :: fid_clipping, fid_original, fid_result
     character(512)  :: filename_clipping, filename_original, filename_result
+    
+    type(Vertex), allocatable       :: clipper_points(:), original_points(:), result_points(:)
+    type(Polygon)                   :: clipper, original, resulting
 
     filename_clipping = ' '
     filename_original = ' '
@@ -20,10 +25,10 @@ program clip_polygon_driver
         call get_command_argument(2, filename_original)
         call get_command_argument(2, filename_result)
     else
-        filename_clipping = './data/clipping_polygon.txt'
-        filename_original = './data/convex_polygon.txt'
-        !filename_original = './data/concave_polygon.txt'
-        filename_result = './data/clipped_polygon.txt'
+        filename_clipping = '../data/clipping_polygon.txt'
+        filename_original = '../data/convex_polygon.txt'
+        !filename_original = '../data/concave_polygon.txt'
+        filename_result = '../data/clipped_polygon.txt'
     end if
 
     open(newunit=fid_clipping, file=trim(adjustl(filename_clipping)))
@@ -31,10 +36,31 @@ program clip_polygon_driver
     open(newunit=fid_result, file=trim(adjustl(filename_result)))
 
     ! read polygons
+    call read_polyfile(fid_clipping, clipper_points)
+    call read_polyfile(fid_original, original_points)
+    call read_polyfile(fid_result, result_points)
+    
+#ifdef _DEBUG
+    call print_list_of_points(clipper_points, 'clipper')
+    call print_list_of_points(original_points, 'original')
+    call print_list_of_points(result_points, 'result')
+#endif    
+    
+    call create_polygon_from_vertices(clipper_points, clipper)
+    call create_polygon_from_vertices(original_points, original)
+    call create_polygon_from_vertices(result_points, resulting)
+    
+#ifdef _DEBUG
+    call print_polygon(clipper, 'clipper')
+    call print_polygon(original, 'original')
+    call print_polygon(resulting, 'result')
+#endif 
 
     ! clip it
 
     ! write results
+    
+    
 
     close(fid_clipping)
     close(fid_original)
@@ -42,5 +68,9 @@ program clip_polygon_driver
 
     write(*,*) ' All done.'
     write(*,*) ' Goodbye'
+    
+#ifdef _DEBUG
+    read(*,*)
+#endif    
 
-end program clip_polygon_driver
+    end program clip_polygon_driver
---
+++ src/polygon_reader.f90
@@ -0,0 +1,102 @@
+
+    module polygon_reader
+    
+    implicit none
+    
+    type Vertex
+        real    :: x
+        real    :: y
+    end type Vertex
+    
+    type Edge
+        type(Vertex) :: start
+        type(Vertex) :: finish
+    end type Edge
+    
+    type Polygon
+        integer                 :: nsides
+        type(Edge), allocatable :: edges(:)
+    end type Polygon
+    
+    contains
+    
+    !*********************************************************
+    subroutine read_polyfile(fid, points)
+    integer, intent(inout)                      :: fid
+    type(Vertex), allocatable, intent(inout)    :: points(:)
+    
+    integer         :: npoints, i
+    integer         :: dummy
+    
+    read(fid, *) npoints
+    allocate(points(npoints))
+    
+    do i = 1, npoints
+        read(fid, *) dummy, points(i).x, points(i).y
+    end do
+    end subroutine read_polyfile
+    
+    !*********************************************************
+    subroutine print_list_of_points(points, message)
+    type(Vertex), allocatable, intent(in)       :: points(:)
+    character(*), intent(in), optional          :: message
+    
+    integer                     :: i, n
+    
+    n = size(points, 1)
+    
+    write(*,'(A)') '---------------------'
+    if (present(message)) then
+        write(*,'(A)') trim(adjustl(message))
+    end if
+    
+    do i = 1, n
+        write(*,'(f8.3,2X,f8.3)') points(i).x, points(i).y 
+    end do
+    
+    write(*,'(A)') '---------------------'
+    end subroutine print_list_of_points
+    
+    !*********************************************************
+    subroutine create_polygon_from_vertices(points, poly)
+    type(Vertex), allocatable, intent(in)       :: points(:)
+    type(Polygon), intent(out)                  :: poly
+    
+    integer                             :: n, i
+    type(Edge), allocatable             :: edges
+    
+    n = size(points, 1)
+    
+    poly.nsides = n
+    allocate(poly.edges(n))
+    
+    do i = 1, n
+        poly.edges(i).start = points(i)
+        poly.edges(i).finish = points(mod(i, n) + 1)
+    end do
+    
+    end subroutine create_polygon_from_vertices
+    
+    !*********************************************************
+    subroutine print_polygon(poly, message)
+    type(Polygon), intent(in)               :: poly
+    character(*), intent(in), optional      :: message
+    
+    integer         :: i
+    
+    write(*,'(A)') '---------------------'
+    if (present(message)) then
+        write(*,'(A)') trim(adjustl(message))
+    end if
+    
+    do i = 1, poly.nsides
+        write(*,'(i0,a,f8.3,a,f8.3,a,f8.3,a,f8.3,a)') i, ' : (', poly.edges(i).start.x, ', ', poly.edges(i).start.y, ')  -->  (', poly.edges(i).finish.x, ', ',  &
+            poly.edges(i).finish.y, ')'
+    end do
+    
+    write(*,'(A)') '---------------------'
+    end subroutine print_polygon
+    
+    end module polygon_reader
+    
+    
